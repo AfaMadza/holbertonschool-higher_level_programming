00f254302a46183663f027b1ee9d5a7453188237
Afa Madza <305@holbertonschool.com>
2018-04-12 21:38:21 +0000

    Infinite add with no malloc...so nice

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-def main():
-    import sys
+import sys
+
+
+def main(argv):
     sum = 0
-    for i in range(1, len(sys.argv)):
-        sum = sum + int(sys.argv[i])
-    print("{:i}".format(sum))
+    for i in range(1, len(argv)):
+        sum = sum + int(argv[i])
+    print("{:d}".format(sum))
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
