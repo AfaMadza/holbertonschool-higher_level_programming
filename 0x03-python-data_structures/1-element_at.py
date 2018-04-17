2999f3b1aba6e7bf7c4918443c081e99e7cbc658
Afa Madza <305@holbertonschool.com>
2018-04-17 23:22:08 +0000

    Removed loop and returned index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
-    count = 0
-    while count < idx:
-        count = count + 1
-    return my_list[count]
+    return my_list[idx]
