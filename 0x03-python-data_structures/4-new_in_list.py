90160ee44ed928f9316da592e602fd6e1396ffcb
Afa Madza <305@holbertonschool.com>
2018-04-17 23:33:47 +0000

    Changed method for returning index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    count = 0
-    while count < idx:
-        count += 1
     list_copy = my_list[:]
-    list_copy[count] = element
+    list_copy[idx] = element
     return list_copy
