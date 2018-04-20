a103e17ae795b24da3bed149ace74f9f427310dd
Afa Madza <305@holbertonschool.com>
2018-04-20 01:04:19 +0000

    Added return None

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,4 +4,5 @@ def update_dictionary(a_dictionary, key, value):
         new_dict = {key: value}
         a_dictionary.update(new_dict)
         return a_dictionary
-    return None
+    else:
+        return None
