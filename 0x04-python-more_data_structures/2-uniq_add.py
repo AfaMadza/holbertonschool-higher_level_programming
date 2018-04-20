82f0659fbd3c67713ed4791c429a9cbb2785f630
Afa Madza <305@holbertonschool.com>
2018-04-20 01:09:01 +0000

    Corrected style errors

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    my_sum = 0;
+    my_sum = 0
     new_list = list(set(my_list))
     for i in range(len(new_list)):
             my_sum += new_list[i]
