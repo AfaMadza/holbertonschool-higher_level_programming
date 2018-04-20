ed762217e585b0353debd42d6edaa3261e9e0d12
Afa Madza <305@holbertonschool.com>
2018-04-20 02:57:05 +0000

    Added return 0 for No string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     from functools import reduce
-    if type(roman_string) is not str or string is None:
+    if type(roman_string) is not str:
+        return 0
+    if roman_string is None:
         return 0
     roman_num = {'I': 1, 'V': 5, 'X': 10,
                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
