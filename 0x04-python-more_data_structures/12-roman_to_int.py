a6b888490186b79dc6950b85928da30f87db9920
Afa Madza <305@holbertonschool.com>
2018-04-20 02:53:14 +0000

    Roman numeral converter with reduce

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    from functools import reduce
+    if type(roman_string) is not str:
+        return None
+    roman_num = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_list = list([])
+    for i in roman_string:
+        for j in roman_num:
+            if i == j:
+                roman_list.append(roman_num[j])
+    func = lambda x, y: x+y if (x >= y) else y-x
+    return(reduce(func, roman_list))
