0d2e3ce4baf2e15f34293dfeefaa4ab25e4efd5f
Afa Madza <305@holbertonschool.com>
2018-04-20 03:49:56 +0000

    Removed import and added error check

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    from functools import reduce
     if type(roman_string) is not str:
         return 0
     if roman_string is None:
         return 0
-    roman_num = {'I': 1, 'V': 5, 'X': 10,
-                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_list = list([])
-    for i in roman_string:
-        for j in roman_num:
-            if i == j:
-                roman_list.append(roman_num[j])
-    func = lambda x, y: x+y if (x >= y) else y-x
-    return(reduce(func, roman_list))
+    roman_num = 0
+    std_not = {'I': 1, 'V': 5, 'X': 10,
+                    'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sub_not = {'IV':4, 'IX':9, 'XL': 40, 'XC':90, 'CD':400, 'CM':900}
+    for x in sub_not.keys():
+        if x in roman_string:
+            roman_num += sub_not[x]
+            roman_string = roman_string.replace(x, "")
+    for x in roman_string:
+            roman_num += std_not[x]
+    return roman_num
