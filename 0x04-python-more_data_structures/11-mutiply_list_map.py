89e921035d715a43f39b08ebbf37a944cd619915
Afa Madza <305@holbertonschool.com>
2018-04-20 00:51:40 +0000

    Reduced code to 3 lines

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    new_list = list(my_list)
-    mult_list = list(map(lambda x: x * number, my_list))
-    return mult_list
+    return(list(map(lambda x: x * number, my_list)))
