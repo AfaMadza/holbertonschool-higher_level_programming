81fa68331e859d246691720861acf72fe8bb562a
Afa Madza <305@holbertonschool.com>
2018-05-09 16:45:29 +0000

    Changed variable name

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,33 +16,33 @@ class TestMaxInteger(unittest.TestCase):
                     self.assertTrue(__import__('6-max_integer').max_integer)
                     def testSimple(self):
                         n_list = [3, 4, 5, 6]
-                        self.assertEqual(max_integer(my_list), 6)
+                        self.assertEqual(max_integer(n_list), 6)
 
                     def testVar(self):
                         i = 69
                         n_list = [1, 5, 3, i]
-                        self.assertEqual(max_integer(my_list), i)
+                        self.assertEqual(max_integer(n_list), i)
                     def testSingle(self):
                         n_list = [7]
-                        self.assertEqual(max_integer(my_list), 7)
+                        self.assertEqual(max_integer(n_list), 7)
                     def testNone(self):
                         n_list = []
-                        self.assertEqual(max_integer(my_list), None)
+                        self.assertEqual(max_integer(n_list), None)
                     def testString(self):
                         n_list = ['x-ray', 'zebra', 'moss']
-                        self.assertEqual(max_integer(my_list), 'zebra')
+                        self.assertEqual(max_integer(n_list), 'zebra')
 
                     def testFailSimple(self):
                         n_list = [1, 3, 'X']
-                        self.assertRaises(TypeError, max_integer, my_list)
+                        self.assertRaises(TypeError, max_integer, n_list)
 
                     def testFalseDict(self):
                         n_list = {'d': 2, 'e': 1, 'f': 3}
-                        self.assertRaises(KeyError, max_integer, my_list)
+                        self.assertRaises(KeyError, max_integer, n_list)
 
                     def testDict(self):
                         n_list = {0: 'b', 1: 'a', 2: 'r'}
-                        self.assertEqual(max_integer(my_list), 'r')
+                        self.assertEqual(max_integer(n_list), 'r')
 
 if __name__ == '__main__':
     unittest.main()
