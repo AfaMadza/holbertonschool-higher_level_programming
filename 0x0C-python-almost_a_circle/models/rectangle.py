749e40e4687af56e4a8b3c5cf19a2623ff1e1ff2
Afa Madza <305@holbertonschool.com>
2018-05-23 00:38:36 +0000

    Fixed pep8 issues

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -48,7 +48,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """str magic method"""
-        return ("[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id,
+        return ("[{}] ({}) {}/{} - {}/{}"
+                .format(type(self).__name__, self.id,
                         self.__x, self.__y, self.__width, self.__height))
 
     def display(self):
