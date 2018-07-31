ab2e589c1b691a4500185d6755b33ec14b7d6182
Afa Madza <305@holbertonschool.com>
2018-07-31 23:14:06 +0000

    Added -s option to curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends GET request and displays response body
-curl $1 -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -s $1 -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
