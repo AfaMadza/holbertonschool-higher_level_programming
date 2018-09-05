2b0769ede4b067ecd719807c86fb39f6916320e5
Afa Madza <305@holbertonschool.com>
2018-09-05 06:30:11 +0000

    Sort dict by value occurence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+function organizeDict (dict) {
+  let objArray = [];
+  let valueList = [];
+  let newKeys = Object.keys(dict).map(function (key) {
+    return dict[key];
+  })
+    .filter(function (elem, index, self) {
+      return index === self.indexOf(elem);
+    });
+  for (let i = 0; i < newKeys.length; i++) {
+    for (let key in dict) {
+      if (newKeys[i] === dict[key]) {
+        valueList.push(key);
+      }
+    }
+    let obj = {};
+    obj[newKeys[i]] = valueList;
+    valueList = [];
+    objArray.push(obj);
+  }
+  console.log(objArray);
+}
+organizeDict(dict);
